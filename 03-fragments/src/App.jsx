4d192490918ab8cec4286f34084839159fb3da2a
yrajput <yuvrajrathore142@gmail.com>
2025-11-05 13:20:35 +0530

    Fragements in react js

---
+++ 03-fragments/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello my name is yuvraj singh rathore</h1>
+      <p>About me: 
+
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, nisi obcaecati aliquid aperiam doloremque nihil aliquam eligendi minus perferendis illo sint, velit iusto dolorem porro autem officia. Et, pariatur libero.
+      </p>
+      <button>Learn more</button>
+    </>
+  )
+}
+
+export default App
