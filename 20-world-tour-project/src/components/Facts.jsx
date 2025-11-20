aab6bee0deda27d7b2f2909b22d9a6b1d7f1de02
yrajput <yuvrajrathore142@gmail.com>
2025-11-20 15:25:35 +0530

    Changes in home page and navigation page

---
+++ 20-world-tour-project/src/components/Facts.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Facts = () => {
+  return (
+    <section className='fact-section'>
+        <div className='heading facts-heading'>
+            <h1>Here are the interesting facts we're proud of</h1>
+        </div>
+
+
+    </section>
+  )
+}
+
+export default Facts
