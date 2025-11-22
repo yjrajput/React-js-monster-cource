5c55d74381035a381741fdfc287838191a886889
yrajput <yuvrajrathore142@gmail.com>
2025-11-22 21:28:49 +0530

    country card complete

--- 20-world-tour-project/src/pages/Country.jsx
+++ 20-world-tour-project/src/pages/Country.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
-
+import React, { useEffect, useState, useTransition } from 'react'
+import { getCountryData } from '../api/postApi'
+import Loader from '../components/Loader';
 const Country = () => {
+  const[isPending, startTransition] = useTransition();
+  const[countrys, setCountrys] = useState([])
+
+  useEffect(()=>{
+    startTransition(async() =>{
+    const res =await getCountryData();
+    setCountrys(res.data);
+    console.log(countrys)
+    })
+  }, [])
+
+  if(isPending) return <Loader/>
+
   return (
-    <div>Country</div>
+    <div className='country-section'>
+        <div className='container country-container'>
+            <div className='country-grid'>
+                
+                  {
+                    countrys.map((items, index) =>(
+                      <div key={index} className='country-card'>
+                          <div className='image-container'>
+                              <img src={items.flags.png} alt="" />
+                          </div>
+
+                          <div className='country-content-container'>
+                              <h3>{items.name.common}</h3>
+                              <p>Population: <span>{items.population}</span></p>
+                              <p>Region: <span>{items.region}</span></p>
+                              <p>Capital: <span>{items.capital}</span></p>
+
+                              <div className='btn-container'>
+                                <button>Read More</button>
+                              </div>
+                          </div>
+                      </div>
+                    ))
+                  }
+                
+            </div>
+        </div>
+    </div>
   )
 }
 
